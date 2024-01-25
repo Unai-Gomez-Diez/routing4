64147f959a5a2ab1e010d4a0b2bc063eb3dba4f1
Unai-Gomez-Diez <unaigomez0807@gmail.com>
2024-01-25 16:56:06 +0100

    avance tocho

--- src/app/aumentar/aumentar.component.ts
+++ src/app/aumentar/aumentar.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Producto } from '../Modelos/producto';
+import { AñadirService } from '../añadir.service';
+import { Route, Router } from '@angular/router';
 
 @Component({
   selector: 'app-aumentar',
@@ -6,5 +9,19 @@ import { Component } from '@angular/core';
   styleUrl: './aumentar.component.css'
 })
 export class AumentarComponent {
+  product: Producto = {
+    id: 0,
+    nombre: '',
+    cantidad: 0,
+    precio: 0.0
+  };
 
+  constructor(private service: AñadirService, private route: Router) {
+
+  }
+  anadirProducto(producto: Producto) {
+    this.service.setProduct(producto)
+    this.service.añadirProduct()
+    this.route.navigate(["/"])
+  }
 }
